9c1860d817549b97f9c92f7f88725b3a706700a4
obaraelijah <elijahobara357@gmail.com>
2023-10-18 19:41:19 +0300

    feat: Configured server

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+import chalk from "chalk";
+import path from "path";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import express from "express";
+import morgan from "morgan";
+import connectionToDB from "./config/connectDB.js";
+import mongoSanitize from "express-mongo-sanitize";
+
+await connectionToDB();
+
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.get("/api/v1/test", (req, res) => {
+	res.json({ Hi: "Welcome to the Invoice App" });
+});
+
+const PORT = process.env.PORT || 1997;
+
+app.listen(PORT, () => {
+	console.log(
+		`${chalk.green.bold("✔")} 👍 Server running in ${chalk.yellow.bold(
+			process.env.NODE_ENV
+		)} mode on port ${chalk.blue.bold(PORT)}`
+	);
+	systemLogs.info(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+	);
+});
