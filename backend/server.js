a19adecc5caf630f8247b15cce95b42c93725957
obaraelijah <elijahobara357@gmail.com>
2023-10-19 19:29:57 +0300

    debug: fix code quality issues

--- backend/server.js
+++ backend/server.js
@@ -5,21 +5,23 @@ import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import connectionToDB from "./config/connectDB.js";
+import { morganMiddleware, systemLogs } from "./utils/Logger.js";
 import mongoSanitize from "express-mongo-sanitize";
 
 await connectionToDB();
 
 const app = express();
 
-if (process.env.NODE_ENV === "development") {
-	app.use(morgan("dev"));
-}
-
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
+
 app.use(cookieParser());
+
+app.use(mongoSanitize());
+
+app.use(morganMiddleware)
 
 app.get("/api/v1/test", (req, res) => {
 	res.json({ Hi: "Welcome to the Invoice App" });
